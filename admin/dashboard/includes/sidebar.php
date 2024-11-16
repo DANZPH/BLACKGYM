b916b9d8e1ace17247fed7b4a70e53567026d247
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-16 15:26:43 +0800

    update admin/dashboard/includes/sidebar.php

--- admin/dashboard/includes/sidebar.php
+++ admin/dashboard/includes/sidebar.php
@@ -1,12 +1,14 @@
 <div id="sidebar-wrapper" class="col-lg-3">
     <div class="container">
-
-
-        <ul class="list-unstyled mt-3 collapse" id="sidebarMenu">
-            <!-- Manage Members Dropdown -->
+        <ul class="list-unstyled mt-3">
+            <!-- Dashboard Link -->
             <li class="nav-item">
-                <a class="nav-link" href="index.php">Dashboard</a>
+                <a class="nav-link" href="index.php">
+                    <i class="fa fa-tachometer-alt"></i> Dashboard
+                </a>
             </li>
+
+            <!-- Manage Members Dropdown -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#manageMembers" data-toggle="collapse" aria-expanded="false">
                     <i class="fa fa-users"></i> Manage Members
@@ -15,7 +17,7 @@
                     <ul class="list-unstyled pl-3">
                         <li><a href="view_member.php" class="nav-link"><i class="fa fa-eye"></i> View Members</a></li>
                         <li><a href="add_member.php" class="nav-link"><i class="fa fa-user-plus"></i> Add Member</a></li>
-                        <li><a href="update_member.php" class="nav-link"><i class="fa fa-edit"></i> Update Members</a></li>
+                        <li><a href="update_member.php" class="nav-link"><i class="fa fa-edit"></i> Update Member</a></li>
                     </ul>
                 </div>
             </li>
