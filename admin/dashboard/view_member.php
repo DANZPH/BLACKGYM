8083b5c43c0621328ed7b0b3e29fca1ee54ca991
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-16 12:49:03 +0800

    update admin/dashboard/view_member.php

--- admin/dashboard/view_member.php
+++ admin/dashboard/view_member.php
@@ -51,9 +51,9 @@ include '../../database/connection.php'; // Include database connection
                 <div class="card-header">
                     <h5>Members Information</h5>
                 </div>
-                <div >
+                <div class="card-body">
                     <!-- Wrap table in a responsive div -->
-                    <div class="card-body" class="table-responsive">
+                    <div class="table-responsive">
                         <table id="membersTable" class="table table-striped table-bordered">
                             <thead>
                                 <tr>
