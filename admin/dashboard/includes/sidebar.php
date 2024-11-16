28e575f6ebef7b2fedc31e120e93ec7f851f1e7a
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-16 15:49:48 +0800

    update admin/dashboard/includes/sidebar.php

--- admin/dashboard/includes/sidebar.php
+++ admin/dashboard/includes/sidebar.php
@@ -1,81 +1,64 @@
-<div id="sidebar-wrapper" class="col-lg-3">
-    <div class="container">
-        <ul class="list-unstyled mt-3">
-            <!-- Dashboard Link -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">
-                    <i class="fa fa-tachometer-alt"></i> Dashboard
-                </a>
-            </li>
-
-            <!-- Manage Members Dropdown -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#manageMembers" data-toggle="collapse" aria-expanded="false">
-                    <i class="fa fa-users"></i> Manage Members
-                </a>
-                <div id="manageMembers" class="collapse">
-                    <ul class="list-unstyled pl-3">
-                        <li><a href="view_member.php" class="nav-link"><i class="fa fa-eye"></i> View Members</a></li>
-                        <li><a href="add_member.php" class="nav-link"><i class="fa fa-user-plus"></i> Add Member</a></li>
-                        <li><a href="update_member.php" class="nav-link"><i class="fa fa-edit"></i> Update Member</a></li>
-                    </ul>
-                </div>
-            </li>
-
-            <!-- Manage Attendance Dropdown -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#manageAttendance" data-toggle="collapse" aria-expanded="false">
-                    <i class="fa fa-calendar-check"></i> Manage Attendance
-                </a>
-                <div id="manageAttendance" class="collapse">
-                    <ul class="list-unstyled pl-3">
-                        <li><a href="checkin_checkout.php" class="nav-link"><i class="fa fa-check-circle"></i> Check-in/Check-out</a></li>
-                        <li><a href="view_attendance.php" class="nav-link"><i class="fa fa-eye"></i> View Attendance</a></li>
-                    </ul>
-                </div>
-            </li>
-
-            <!-- Payment Dropdown -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#managePayments" data-toggle="collapse" aria-expanded="false">
-                    <i class="fa fa-credit-card"></i> Payment
-                </a>
-                <div id="managePayments" class="collapse">
-                    <ul class="list-unstyled pl-3">
-                        <li><a href="pay_cancel.php" class="nav-link"><i class="fa fa-money-bill"></i> Pay/Cancel</a></li>
-                    </ul>
-                </div>
-            </li>
-
-            <!-- Reports Dropdown -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#reports" data-toggle="collapse" aria-expanded="false">
-                    <i class="fa fa-chart-bar"></i> Report
-                </a>
-                <div id="reports" class="collapse">
-                    <ul class="list-unstyled pl-3">
-                        <li><a href="chart_report.php" class="nav-link"><i class="fa fa-chart-line"></i> Chart Representation</a></li>
-                        <li><a href="members_report.php" class="nav-link"><i class="fa fa-users"></i> Members Report</a></li>
-                    </ul>
-                </div>
-            </li>
-
-            <!-- Transaction Dropdown -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#transactions" data-toggle="collapse" aria-expanded="false">
-                    <i class="fa fa-exchange-alt"></i> Transaction
-                </a>
-                <div id="transactions" class="collapse">
-                    <ul class="list-unstyled pl-3">
-                        <li><a href="payment_transaction.php" class="nav-link"><i class="fa fa-credit-card"></i> Payment</a></li>
-                        <li><a href="receipt_transaction.php" class="nav-link"><i class="fa fa-file-invoice"></i> Receipt</a></li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div>
-
-<div class="content-wrapper">
-    <!-- Content Goes Here -->
+<div class="sidebar">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">
+                <i class="fa fa-tachometer-alt"></i> Dashboard
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#manageMembers" data-toggle="collapse" aria-expanded="false">
+                <i class="fa fa-users"></i> Manage Members
+            </a>
+            <div id="manageMembers" class="collapse">
+                <ul class="nav flex-column ml-3">
+                    <li class="nav-item"><a href="view_member.php" class="nav-link">View Members</a></li>
+                    <li class="nav-item"><a href="add_member.php" class="nav-link">Add Member</a></li>
+                    <li class="nav-item"><a href="update_member.php" class="nav-link">Update Member</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#manageAttendance" data-toggle="collapse" aria-expanded="false">
+                <i class="fa fa-calendar-check"></i> Manage Attendance
+            </a>
+            <div id="manageAttendance" class="collapse">
+                <ul class="nav flex-column ml-3">
+                    <li class="nav-item"><a href="checkin_checkout.php" class="nav-link">Check-in/Check-out</a></li>
+                    <li class="nav-item"><a href="view_attendance.php" class="nav-link">View Attendance</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#managePayments" data-toggle="collapse" aria-expanded="false">
+                <i class="fa fa-credit-card"></i> Payments
+            </a>
+            <div id="managePayments" class="collapse">
+                <ul class="nav flex-column ml-3">
+                    <li class="nav-item"><a href="pay_cancel.php" class="nav-link">Pay/Cancel</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#reports" data-toggle="collapse" aria-expanded="false">
+                <i class="fa fa-chart-bar"></i> Reports
+            </a>
+            <div id="reports" class="collapse">
+                <ul class="nav flex-column ml-3">
+                    <li class="nav-item"><a href="chart_report.php" class="nav-link">Chart Representation</a></li>
+                    <li class="nav-item"><a href="members_report.php" class="nav-link">Members Report</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#transactions" data-toggle="collapse" aria-expanded="false">
+                <i class="fa fa-exchange-alt"></i> Transactions
+            </a>
+            <div id="transactions" class="collapse">
+                <ul class="nav flex-column ml-3">
+                    <li class="nav-item"><a href="payment_transaction.php" class="nav-link">Payments</a></li>
+                    <li class="nav-item"><a href="receipt_transaction.php" class="nav-link">Receipts</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
 </div>
