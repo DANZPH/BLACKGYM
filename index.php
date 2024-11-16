3265777c823d67e119622467d1352686559db0e7
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-17 00:19:30 +0800

    update index.php

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ position: fixed;
   <div class="footer">
     <p>&copy; 2024 BLACKGYM. All Rights Reserved. 
       <a href="https://github.com/DANZPH/BLACK-GYM" target="_blank">
-        <i class="fab fa-github"></i> GitHub Repo - Collaborate with Us
+        <i class="fab fa-github"></i> GitHub Repo - Collaborate with Me
       </a>
     </p>
   </div>
