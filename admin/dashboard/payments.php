4f754c6a04b526776e403b78c42bb288b88fe337
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-17 22:24:39 +0800

    update admin/dashboard/payments.php

--- admin/dashboard/payments.php
+++ admin/dashboard/payments.php
@@ -116,6 +116,7 @@ include '../../database/connection.php'; // Include database connection
     </div>
 </div>
 
+<!-- jQuery and Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
@@ -209,14 +210,4 @@ include '../../database/connection.php'; // Include database connection
                         $('#paymentModal').modal('hide');
                         location.reload();
                     },
-                    error: function () {
-                        alert('An error occurred. Please try again.');
-                    }
-                });
-            }
-        });
-    });
-</script>
-
-</body>
-</html>
+                    error: function ()
