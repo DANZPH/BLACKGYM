84afa6279d005b199b7cf2ecfd1bb26ea6df4e5a
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-18 01:04:11 +0800

    update admin/dashboard/payments.php

--- admin/dashboard/payments.php
+++ admin/dashboard/payments.php
@@ -83,8 +83,8 @@ include '../../database/connection.php'; // Include database connection
                 </div>
             </div>
         </div>
+    <?php include '../../includes/footer.php'; ?>
     </div>
-        <?php include '../../includes/footer.php'; ?>
 </div>
 
 <!-- Modal for Payment -->
