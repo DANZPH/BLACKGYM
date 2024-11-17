c20c41861a753386902e6e2cd49ad4089f4fb685
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-18 00:56:53 +0800

    update admin/dashboard/payments.php

--- admin/dashboard/payments.php
+++ admin/dashboard/payments.php
@@ -124,6 +124,7 @@ include '../../database/connection.php'; // Include database connection
       </div>
     </div>
   </div>
+      <?php include '../includes/footer.php'; ?>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
