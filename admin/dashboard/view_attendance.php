cad26a4e25df4a1e0fe72278eea13da13a38423c
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-18 01:07:05 +0800

    update admin/dashboard/view_attendance.php

--- admin/dashboard/view_attendance.php
+++ admin/dashboard/view_attendance.php
@@ -10,20 +10,8 @@ include '../../database/connection.php'; // Include database connection
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Members Attendance</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- DataTables CSS -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
-    <!-- FontAwesome CSS -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <!-- Custom Styles -->
-    <link rel="stylesheet" href="includes/styles.css">
-</head>
+<html lang="en"><?php include '../../includes/head.php';?>
+
 
 <body>
 
