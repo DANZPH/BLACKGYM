3474a888aee69d1ce3480b2bd9d9883edb5640a3
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-18 00:54:25 +0800

    update admin/dashboard/index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -29,14 +29,8 @@ $pendingPayments = $pendingPaymentsResult->fetch_assoc()['pending_payments'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="includes/styles.css">
-</head>
+                  <?php include '../../includes/head.php'; ?>
+
 <body>
     <?php include 'includes/sidebar.php'; ?>
 
