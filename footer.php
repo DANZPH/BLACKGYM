84742b43d9e7e80fc5593af656caa9ea5d72e8f3
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-18 00:44:35 +0800

    update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
  <div class="footer">
-    <p>&copy; 2024 BLACKGYM. All Rights Reserved. 
+    <p>&copy; 2024 BLACKGYM. All Rights Reserved.. 
       <a href="https://github.com/DANZPH/BLACK-GYM" target="_blank">
         <i class="fab fa-github"></i> GitHub Repo - Collaborate with Me
       </a>
