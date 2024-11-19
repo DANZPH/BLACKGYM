b671f3a88004fb5dae2a6546725bb76fd1943083
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 10:58:56 +0800

    update admin/dashboard/includes/header.php

--- admin/dashboard/includes/header.php
+++ admin/dashboard/includes/header.php
@@ -1,13 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="index.php">Admin Dashboard</a>
-    
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-navbar">
-    <button id="sidebarToggle" class="btn btn-light mr-3">
-        <i class="fas fa-bars"></i>
-    </button>
-    <a class="navbar-brand" href="#">Admin Dashboard</a>
-</nav>
-    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
