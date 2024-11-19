845b1444bc19a7e33c6690850c23ff1463e6a018
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 10:39:18 +0800

    update admin/dashboard/index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -88,9 +88,5 @@ $pendingPayments = $pendingPaymentsResult->fetch_assoc()['pending_payments'];
             <?php include '../../includes/footer.php'; ?>
         </div>
     </div>
-
-    <!-- JavaScript -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
