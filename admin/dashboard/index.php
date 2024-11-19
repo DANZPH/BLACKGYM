b61e4fd22b4bdba28660d2c2a0a459d14c2d97c5
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 11:43:12 +0800

    update admin/dashboard/index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -89,13 +89,7 @@ $pendingPayments = $pendingPaymentsResult->fetch_assoc()['pending_payments'];
         </div>
 
     </div>
-<div class="footer">
-    <p>&copy; 2024 BLACKGYM. All Rights Reserved.
-        <a href="https://github.com/DANZPH/BLACK-GYM" target="_blank">
-            <i class="fab fa-github"></i> GitHub Repo - Collaborate with Me
-        </a>
-    </p>
-</div>
+                    <?php include '../../includes/footer.php'; ?>
 
 
     <!-- JavaScript -->
