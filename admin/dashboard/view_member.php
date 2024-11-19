c6345b3eb2764797d8042eb7740e416e458d8b77
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 12:22:45 +0800

    update admin/dashboard/view_member.php

--- admin/dashboard/view_member.php
+++ admin/dashboard/view_member.php
@@ -88,11 +88,10 @@ include '../../database/connection.php'; // Include database connection
                     </div>
                 </div>
             </div>
-    <?php include '../../includes/footer.php'; ?>
         </div>
     </div>
 </div>
-
+    <?php include '../../includes/footer.php'; ?>
 <!-- Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
