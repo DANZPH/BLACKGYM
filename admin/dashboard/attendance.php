3f1cf9d094a6eda79a03d49b386a064c098101e0
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 12:18:09 +0800

    update admin/dashboard/attendance.php

--- admin/dashboard/attendance.php
+++ admin/dashboard/attendance.php
@@ -107,9 +107,8 @@ include '../../database/connection.php';
 
         </div>
     </div>
-    <?php include '../../includes/footer.php'; ?>
 </div>
-
+    <?php include '../../includes/footer.php'; ?>
 <!-- Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
