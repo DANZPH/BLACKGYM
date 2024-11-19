4ab640d8709089263fd3793d7a8659c77ca46aca
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 12:29:54 +0800

    update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BLACKGYM</title>
-<link href="admin/dashboard/includes/css/styles.css"
+<link href="admin/dashboard/includes/styles.css"
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
