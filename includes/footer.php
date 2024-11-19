d61158dd251d86d800bdeb50de8783c27b7a2e1d
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-19 12:38:40 +0800

    update includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
  <div class="footer">
-    <p>&copy; 2024 BLACKGYM. All Rights Reserved.
+    <p>&copy; 2024 DANCEL. All Rights Reserved.
       <a style="color: #7fffee;" href="https://github.com/DANZPH/BLACK-GYM" target="_blank">
         <i class="fab fa-github"></i> GitHub Repo - Collaborate with Me
       </a>
