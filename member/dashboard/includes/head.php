0dd7a1f1ab0fe3c450d76f9269db5cee1ade9ccb
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-20 20:07:28 +0800

    update member/dashboard/includes/head.php

---
+++ member/dashboard/includes/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Member Login</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- SweetAlert2 CSS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
