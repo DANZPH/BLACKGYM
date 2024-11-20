ecb0e4a76d9e316c23f67b7d19536b3dc827c92f
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-20 18:35:53 +0800

    update includes/head.php

--- includes/head.php
+++ includes/head.php
@@ -6,5 +6,4 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="includes/styles.css">
 <!--        <link rel="stylesheet" href="../../css/footer.css">-->
-
 </head>
