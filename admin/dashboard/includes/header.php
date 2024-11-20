ac428c2c97b0717144d7435abb8e2d18366742cf
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-20 18:37:32 +0800

    update admin/dashboard/includes/header.php

--- admin/dashboard/includes/header.php
+++ admin/dashboard/includes/header.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="index.php">Admin Dashboard</a>
-              <p class="nightowl-daylight"></p>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
