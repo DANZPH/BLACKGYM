84ac71ae252265a83c7f5fde5cf3b1ead1fbead1
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-20 18:26:31 +0800

    update includes/head.php

--- includes/head.php
+++ includes/head.php
@@ -6,4 +6,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="includes/styles.css">
 <!--        <link rel="stylesheet" href="../../css/footer.css">-->
+<script
+    type="module"
+    src="https://cdn.jsdelivr.net/npm/@bufferhead/nightowl@0.0.14/dist/nightowl.js"
+></script>
 </head>
