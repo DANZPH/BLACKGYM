6bd8a0d2387008f617daa1d2dbe02d3a55227afa
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-22 11:14:14 +0800

    update index.php

--- index.php
+++ index.php
@@ -19,12 +19,13 @@
 
   <style>
     /* Custom Styles for Dark Theme */
-    body {
-      background-color: #121212;
-      color: #fff;
-      font-family: 'Roboto', sans-serif;
-      margin: 0;
-      padding-top: 30px;
+    body {background: linear-gradient(90deg, #bdc3c7,  #2c3e50);         
+    padding-top: 30px;
+    height: 100%;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+      
     }
 
     .navbar {
