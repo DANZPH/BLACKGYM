977e15b46975e3e8e5f0d9f3b71cd266739e98c7
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 01:21:42 +0800

    update login/register.php

--- login/register.php
+++ login/register.php
@@ -1,13 +1,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Registration</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-</head>
+<?php include 'dashboard/includes/head.php'; ?>
 <body>
     <div class="container mt-5">
         <div class="row justify-content-center">
