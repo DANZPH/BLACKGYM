bb336e5f53f542ff7259df31c822538bb170a20f
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 01:37:08 +0800

    update admin/login.php

--- admin/login.php
+++ admin/login.php
@@ -25,11 +25,11 @@ if (isset($_SESSION['AdminID'])) {
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <div class="card">
-                    <div style="border-radius: 8px;
+                <div style="border-radius: 8px;
       margin-bottom: 1.5rem;
       background: #fff;
-      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);"class="card-header h2">Admin Login</div>
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);" class="card">
+                    <div class="card-header h2">Admin Login</div>
                     <div class="card-body">
                         <form action="action/login_process.php" method="POST">
                             <div class="form-group">
