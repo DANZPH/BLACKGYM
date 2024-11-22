70751eb93a8a993b3a8a763dd19952a6cff5cf95
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 01:20:14 +0800

    update member/dashboard/includes/head.php

--- member/dashboard/includes/head.php
+++ member/dashboard/includes/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Member Login</title>
+    <title>Member</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- SweetAlert2 CSS -->
