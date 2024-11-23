da60e03594117a019646718ab908719e1eae2e49
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 20:03:10 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -4,12 +4,8 @@ if (!isset($_SESSION['MemberID'])) {
     header('Location: login.php');
     exit();
 }
-
-include '../../database/connection.php'; // Include database connection
-
-$memberID = $_SESSION['MemberID']; // Get the logged-in member's ID
-
-// Query to fetch the membership details (EndDate, Status)
+include '../../database/connection.php';
+$memberID = $_SESSION['MemberID'];
 $sql = "SELECT EndDate, Status FROM Membership WHERE MemberID = ?";
 $stmt = $conn1->prepare($sql);
 $stmt->bind_param("d", $memberID);
@@ -107,13 +103,11 @@ if ($endDate) {
                 <?php else: ?>
                     <p>Your membership is still pending. Please PAY amd wait for approval.</p>
                 <?php endif; ?>
-
-                <!-- Add more content as needed -->
             </div>
         </div>
     </div>
-                    <?php include '../../includes/footer.php'; ?>
-    <!-- Bootstrap JS, Popper.js, and jQuery -->
+<!--                    <?php include '../../includes/footer.php'; ?>-->
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
