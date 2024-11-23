80c3efa8627b35f33fe891395aec16858c3cd25a
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 17:24:34 +0800

    update admin/dashboard/includes/header.php

--- admin/dashboard/includes/header.php
+++ admin/dashboard/includes/header.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand " href="index.php">BLACK GYM</a>
-    <button class="sidebar-toggle" id="sidebarToggle">☰</button>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
