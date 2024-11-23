1c0bb30cd2c05f15f5ac7da893668e47195252f3
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 20:01:36 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -112,7 +112,7 @@ if ($endDate) {
             </div>
         </div>
     </div>
-                    <?php include '../includes/footer.php'; ?>
+                    <?php include '../../includes/footer.php'; ?>
     <!-- Bootstrap JS, Popper.js, and jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
