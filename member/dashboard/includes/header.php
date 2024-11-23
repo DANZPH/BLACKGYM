e6bfbc517c178685549c27e84ccebe35f0e32970
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 19:58:56 +0800

    update member/dashboard/includes/header.php

--- member/dashboard/includes/header.php
+++ member/dashboard/includes/header.php
@@ -3,7 +3,7 @@
 session_start();
 ?>
 
-<header class="navbar navbar-expand-lg navbar-dark bg-dark">
+<header class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
         <a class="navbar-brand" href="index.php">Dashboard</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
