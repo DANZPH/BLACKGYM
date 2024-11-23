dd16c00f5e56d849fcfabf081911ecff90be15f9
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 16:51:35 +0800

    update includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
- <footer class="footer">
+ <footer class="footer fixed-bottom">
     <p>&copy; 2024 DANCEL. All Rights Reserved.
       <a style="color: #7fffee;" href="https://github.com/DANZPH/BLACK-GYM" target="_blank"><br>
         <i class="fab fa-github"></i> GitHub Repository
