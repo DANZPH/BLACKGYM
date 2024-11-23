b7792f782f5ce303d4048c6dabb3a267ef9d143a
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 20:11:23 +0800

    update member/dashboard/includes/header.php

--- member/dashboard/includes/header.php
+++ member/dashboard/includes/header.php
@@ -5,6 +5,10 @@ session_start();
 
 <header class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
+      
+          <button class="btn btn-primary d-md-none" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+        <span class="navbar-toggler-icon"></span> Menu
+    </button>
         <a class="navbar-brand" href="index.php">Dashboard</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
