ec47fe87af34eca6b1523b5a78f3fb41b76458c5
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 20:12:38 +0800

    update member/dashboard/includes/sidebar.php

--- member/dashboard/includes/sidebar.php
+++ member/dashboard/includes/sidebar.php
@@ -1,9 +1,5 @@
 <!-- sidebar.php -->
 <div class="col-md-3 bg-light">
-    <!-- Toggle button for small screens -->
-
-    
-    <!-- Collapsible sidebar menu -->
     <div class="collapse d-md-block" id="sidebar">
         <div class="list-group">
             <a href="index.php" class="list-group-item list-group-item-action">Dashboard Home</a>
