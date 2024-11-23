1598927f4e9f29fcd5fbe4c2edbc0cf7137ad792
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 20:24:46 +0800

    update member/dashboard/includes/header.php

--- member/dashboard/includes/header.php
+++ member/dashboard/includes/header.php
@@ -6,7 +6,7 @@ session_start();
 <header class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
       
-          <button class="btn d-md-none" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+          <button class="btn glyphicon glyphicon-chevron-down d-md-none" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
         <span class="navbar-toggler-icon"></span>
     </button>
         <a class="navbar-brand" href="index.php">Dashboard</a>
