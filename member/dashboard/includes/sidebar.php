7a291ef339e33b7a901e63400c93ed9ad0ed7512
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-23 20:09:40 +0800

    update member/dashboard/includes/sidebar.php

--- member/dashboard/includes/sidebar.php
+++ member/dashboard/includes/sidebar.php
@@ -1,11 +1,19 @@
 <!-- sidebar.php -->
 <div class="col-md-3 bg-light">
-    <div class="list-group">
-        <a href="index.php" class="list-group-item list-group-item-action">Dashboard Home</a>
-        <a href="profile.php" class="list-group-item list-group-item-action">Profile</a>
-        <a href="payments.php" class="list-group-item list-group-item-action">Payments</a>
-        <a href="attendance.php" class="list-group-item list-group-item-action">Attendance</a>
-        <a href="membership.php" class="list-group-item list-group-item-action">Membership</a>
-        <a href="announcements.php" class="list-group-item list-group-item-action">Announcements</a>
+    <!-- Toggle button for small screens -->
+    <button class="btn btn-primary d-md-none" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+        <span class="navbar-toggler-icon"></span> Menu
+    </button>
+    
+    <!-- Collapsible sidebar menu -->
+    <div class="collapse d-md-block" id="sidebar">
+        <div class="list-group">
+            <a href="index.php" class="list-group-item list-group-item-action">Dashboard Home</a>
+            <a href="profile.php" class="list-group-item list-group-item-action">Profile</a>
+            <a href="payments.php" class="list-group-item list-group-item-action">Payments</a>
+            <a href="attendance.php" class="list-group-item list-group-item-action">Attendance</a>
+            <a href="membership.php" class="list-group-item list-group-item-action">Membership</a>
+            <a href="announcements.php" class="list-group-item list-group-item-action">Announcements</a>
+        </div>
     </div>
 </div>
