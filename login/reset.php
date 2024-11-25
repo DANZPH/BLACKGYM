a698d1acc3800f1aa9e83a68efab7abdefc2e73b
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-26 00:30:39 +0800

    update login/reset.php

--- login/reset.php
+++ login/reset.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["email"]) && isset($_GET[
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Reset Password</title>
+            <title>Reset-Password</title>
             <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.18/dist/sweetalert2.min.css" rel="stylesheet">
             <style>
                 body {
