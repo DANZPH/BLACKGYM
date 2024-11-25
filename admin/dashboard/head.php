1c93c0ed3b4e99ffe9efd08b98f2b0064f78ca80
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-25 21:30:50 +0800

    update admin/dashboard/head.php

--- admin/dashboard/head.php
+++ admin/dashboard/head.php
@@ -5,6 +5,6 @@
       <link rel="icon" href="../../img/favicon-512x512.png" sizes="512x512" type="image/png">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="includes/styles.css">
+    <link rel="stylesheet" href="../includes/styles.css">
 <!--        <link rel="stylesheet" href="../../css/footer.css">-->
 </head>
