e4ee3e06a728d73deea99bcf4bfe95e736419b2e
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-27 23:03:07 +0800

    update member/action/qr.php

--- member/action/qr.php
+++ member/action/qr.php
@@ -1,5 +1,6 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1); session_start();
+error_reporting(E_ALL); ini_set('display_errors', 1); 
+session_start();
 include '../../database/connection.php';
 
 // Check if the user is logged in
