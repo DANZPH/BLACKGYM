45e387209eed53d9c6601858837c3a6c895c00c0
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-27 22:22:43 +0800

    update admin/action/t.php

--- admin/action/t.php
+++ admin/action/t.php
@@ -1,5 +1,5 @@
 <?php
-include '../../database/connection.php';
+
 require_once '../../vendor/autoload.php'; // Ensure the autoloader is included
 
 use Endroid\QrCode\Builder\Builder;
@@ -7,16 +7,13 @@ use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\Writer\PngWriter;
 
-$query = "SELECT ReceiptNumber FROM Payments ORDER BY PaymentDate DESC LIMIT 1";
-    $result = $conn1->query($query);
-    $row = $result->fetch_assoc();
-        $receiptNumber = $row['ReceiptNumber'];
+
     // Initialize the builder (no logo or label)
     $builder = new Builder(
         writer: new PngWriter(),
         writerOptions: [],
         validateResult: false,
-        data: $receiptNumber, // Content of the QR code (this can be any string)
+        data: 'Custom QR code contents', // Content of the QR code (this can be any string)
         encoding: new Encoding('UTF-8'),
         errorCorrectionLevel: ErrorCorrectionLevel::High, // High error correction
         size: 300, // Size of the QR code (pixels)
