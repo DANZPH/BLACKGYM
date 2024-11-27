53352423d946f41ac3b7897d160324bf89f60fb7
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-27 23:55:40 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -182,7 +182,7 @@ if ($endDate) {
             </div>
         </div>
     </div>
-<script src="script.js"></script>
+<script type="text/javascript"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
