7f5965893ce93b8ec72f05ab59d972f7abf7dad2
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-27 23:56:48 +0800

    update member/dashboard/script.js

--- member/dashboard/script.js
+++ member/dashboard/script.js
@@ -1,14 +1,13 @@
-<script type="text/javascript">
-    // Generate the QR code if there's a receipt number
-    var receiptNumber = "<?php echo $latestReceiptNumber; ?>";
-    if (receiptNumber) {
-        var qrcode = new QRCode(document.getElementById("qrcode"), {
-            text: receiptNumber,  // Set the text to the receipt number
-            width: 128,           // Width of the QR code
-            height: 128,          // Height of the QR code
-            colorDark : "#000000", // Dark color
-            colorLight : "#ffffff", // Light color
-            correctLevel : QRCode.CorrectLevel.H // Error correction level
-        });
-    }
-</script>
+// Check if receiptNumber is available (from the PHP code passed to window object)
+if (window.receiptNumber) {
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: window.receiptNumber,  // Set the text to the receipt number
+        width: 128,                  // Width of the QR code
+        height: 128,                 // Height of the QR code
+        colorDark: "#000000",        // Dark color
+        colorLight: "#ffffff",       // Light color
+        correctLevel: QRCode.CorrectLevel.H // Error correction level
+    });
+} else {
+    document.getElementById("qrcode").innerHTML = "No receipt number found.";
+}
