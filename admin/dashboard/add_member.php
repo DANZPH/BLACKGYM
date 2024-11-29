838e0425553e9d861ef1191f573454b41c23c056
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-11-29 17:08:53 +0800

    update admin/dashboard/add_member.php

--- admin/dashboard/add_member.php
+++ admin/dashboard/add_member.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Registration</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../admin/dashboard/includes/styles.css">
-</head>
+<?php include '../../includes/head.php'; ?>
 <body>
+      <?php include 'includes/sidebar.php'; ?>
+          <div class="content-wrapper">
+        <?php include 'includes/header.php'; ?>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
