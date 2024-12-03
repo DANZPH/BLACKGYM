5d17f90432e50dfb6cf6eb50677ceeff0286db13
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-04 00:37:59 +0800

    update admin/action/add_member_process.php

--- admin/action/add_member_process.php
+++ admin/action/add_member_process.php
@@ -1,12 +1,5 @@
 <?php
-echo 'Current PHP timezone: ' . date_default_timezone_get();
 
-
-// Set the timezone to Asia/Manila
-date_default_timezone_set('Asia/Manila');
-
-// Example usage of date and time functions
-echo date('Y-m-d H:i:s');  // Outputs the current date and time in Manila timezone
 require '../../login/phpmailer/src/Exception.php';
 require '../../login/phpmailer/src/PHPMailer.php';
 require '../../login/phpmailer/src/SMTP.php';
