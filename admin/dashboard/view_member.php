44e7e12ae83e8c2469719247a983aac03facb253
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-04 18:14:47 +0800

    update admin/dashboard/view_member.php

--- admin/dashboard/view_member.php
+++ admin/dashboard/view_member.php
@@ -137,11 +137,11 @@ include '../../includes/head.php';
                                             <td>{$row['MembershipStatus']}</td>
                                             <td>{$row['created_at']}</td>
                                           <td>
-    <div class='d-flex gap-2'>
+    <div style="gap: 10px;" class='d-flex gap-2'>
         <button class='btn btn-warning btn-sm editBtn' data-id='{$row['MemberID']}'>
             <i class='fas fa-edit'></i> Edit
         </button>
-        <p>
+        
         <button class='btn btn-danger btn-sm deleteBtn' data-id='{$row['MemberID']}'>
             <i class='fas fa-trash'></i> Delete
         </button>
