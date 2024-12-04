9e56e750772e70d3341c10fd0bac9e29cc917747
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-04 12:01:18 +0800

    update admin/dashboard/view_member.php

--- admin/dashboard/view_member.php
+++ admin/dashboard/view_member.php
@@ -154,12 +154,6 @@ include '../../includes/head.php';
 
 <?php include '../../includes/footer.php'; ?>
 
-<!-- JS Scripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="includes/JS/add_member.js"></script>
 </body>
 </html>
