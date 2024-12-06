3af0524df9887d5d4b47cc1bf7d1c35c314f5c35
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-06 14:35:51 +0800

    update admin/dashboard/includes/header.php

--- admin/dashboard/includes/header.php
+++ admin/dashboard/includes/header.php
@@ -4,6 +4,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <!-- JS Scripts -->
+    <script src="includes/JS/sweetalert.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
