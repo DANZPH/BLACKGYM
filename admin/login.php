edaeba36c098d0b6ac0c63a549c46bfab5e11e27
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-06 18:28:29 +0800

    update admin/login.php

--- admin/login.php
+++ admin/login.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['AdminID'])) {
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <div class="card">
+                <div class="card text-center">
                     <div class="card-header h2 text-center">Admin Login</div>
                     <div class="card-body">
                         <form action="action/login_process.php" method="POST">
