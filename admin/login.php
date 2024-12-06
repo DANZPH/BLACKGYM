105dab8c4a1a8129d4d56b072348a229da967f77
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-06 18:31:16 +0800

    update admin/login.php

--- admin/login.php
+++ admin/login.php
@@ -23,9 +23,11 @@ if (isset($_SESSION['AdminID'])) {
 <link rel="stylesheet" href="dashboard/includes/Loginform.css">
 </head>
 <body>
+      <div class="content-wrapper">
+        <?php include 'dashboard/includes/header.php'; ?>
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-6 justify-content-center">
+            <div class="col-md-6">
                 <div class="card">
                     <div class="card-header h2 text-center">Admin Login</div>
                     <div class="card-body">
