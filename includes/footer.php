e0a6b21845fb67422d82c648df201e4bd5b5e295
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-06 15:05:03 +0800

    update includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,11 @@
  <footer class="footer fixed-bottom">
-    <p>&copy; 2024 DANCEL.
+    <p>&copy; 2024 DANCEL. 
       <a style="color: #7fffee;" href="https://github.com/DANZPH/BLACKGYM" target="_blank">
         <i class="fab fa-github"></i> GitHub Repository
       </a>
     </p>
-  </footer>
+    <p>
+      <i class="fas fa-dumbbell"></i> <!-- Example icon for brand (gym-related) -->
+      Powered by BLACKGYM
+    </p>
+</footer>
