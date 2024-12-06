e8944645716a868b7f75f0e28934def72661f830
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-06 14:38:25 +0800

    update admin/dashboard/includes/header.php

--- admin/dashboard/includes/header.php
+++ admin/dashboard/includes/header.php
@@ -3,8 +3,15 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <!-- JS Scripts -->
+<!--    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            </li>
+        </ul>
+    </div>-->
     <script src="includes/JS/sweetalert.js"></script>
+  <script src="includes/JS/script.js"></script>
+    <!-- JS Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
