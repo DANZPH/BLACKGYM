5d5d6b0a5a73470cce29e12f30abbe84ea32234b
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-06 18:18:53 +0800

    update admin/login.php

--- admin/login.php
+++ admin/login.php
@@ -11,6 +11,7 @@ if (isset($_SESSION['AdminID'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+  <?php include '../includes/head.php'; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
