5dc47b2042f3d01e93524424e1b2cab489e9b252
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-07 23:00:01 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -2,11 +2,11 @@
 session_start();
 
 // Check if the user is logged in and the MemberID session is set
-/*if (!isset($_SESSION['member_id']) || !isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['member_id']) || !isset($_SESSION['user_id'])) {
     header('Location: ../login.php');
     exit();
 }
-*/
+
 include '../../database/connection.php';
 
 // Fetch the MemberID from the session
