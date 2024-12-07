606c2286121cc89fd2a809616e3b12284ff87424
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-07 23:05:34 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -2,8 +2,8 @@
 session_start();
 
 // Check if the user is logged in and the MemberID session is set
-if (!isset($_SESSION['MemberID'])) {
-    header('Location: login.php');
+if (!isset($_SESSION['member_id']) || !isset($_SESSION['user_id'])) {
+    header('Location: ../login.php');
     exit();
 }
 
