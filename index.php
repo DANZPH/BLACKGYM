7511f144cf205a281f2162eb2921fe99e4fcb173
DANZPH <kentdancel2003@gmail.com>
2024-12-09 11:04:54 +0800

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>BLACKGYM  page</title>
+  <title>BLACKGYM  Page</title>
   <link rel="icon" href="img/favicon-512x512.png" sizes="512x512" type="image/png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
   
