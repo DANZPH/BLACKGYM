62633db5c024426174a60ca7965eedc3353995b4
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-10 12:04:16 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -38,7 +38,6 @@ if ($endDate) {
     <title>Member Dashboard</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    	<link rel="stylesheet" href="includes/style.css">
     <style>
         html, body {
   background-color: #bbbbbb;
@@ -186,7 +185,7 @@ if ($endDate) {
             </div>
         </div>
     </div>
-	<script src="script.js"></script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
