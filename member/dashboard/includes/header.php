b115d210c01123eb745dc953223c4dff4d4fd0ed
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-10 12:27:31 +0800

    update member/dashboard/includes/header.php

--- member/dashboard/includes/header.php
+++ member/dashboard/includes/header.php
@@ -1,27 +1,24 @@
-<!-- header.php -->
 <?php
 session_start();
 ?>
-
-<header class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      
-          <button class="btn d-md-none" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-        <a class="navbar-brand" href="index.php">Dashboard</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="../action/fetch_receipt.php">Hello, <?php echo $_SESSION['username']; ?></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../action/logout.php">Logout</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</header>
+	<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu' ></i>
+			<a href="#" class="nav-link">Categories</a>
+			<form action="#">
+				<div class="form-input">
+					<input type="search" placeholder="Search...">
+					<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+				</div>
+			</form>
+			<input type="checkbox" id="switch-mode" hidden>
+			<label for="switch-mode" class="switch-mode"></label>
+			<a href="#" class="notification">
+				<i class='bx bxs-bell' ></i>
+				<span class="num">8</span>
+			</a>
+			<a href="#" class="profile">
+				<img src="img/people.png">
+			</a>
+		</nav>
+		<!-- NAVBAR -->
