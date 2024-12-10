d19a6796bbd7c86fc016409c16d73cf6182b1799
DANZPH <kentdancel2003@gmail.com>
2024-12-10 14:01:42 +0800

    update index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -34,6 +34,7 @@ if ($endDate) {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 <!-- SweetAlert2 -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<!-- Boxicons -->
@@ -255,5 +256,11 @@ include 'includes/header.php';
     }
 </script>
 	<script src="includes/script.js"></script>
+	
+	
+	
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
