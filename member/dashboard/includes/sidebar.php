2a84bcc6ceeb6b6ebca7151b515a3a92977c2299
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-10 11:59:19 +0800

    update member/dashboard/includes/sidebar.php

--- member/dashboard/includes/sidebar.php
+++ member/dashboard/includes/sidebar.php
@@ -1,13 +1,55 @@
-<!-- sidebar.php -->
-<div class="col-md-3">
-    <div class="collapse d-md-block" id="sidebar">
-        <div class="list-group">
-            <a href="index.php" class="list-group-item list-group-item-action">Dashboard Home</a>
-            <a href="profile.php" class="list-group-item list-group-item-action">Profile</a>
-            <a href="payments.php" class="list-group-item list-group-item-action">Payments</a>
-            <a href="attendance.php" class="list-group-item list-group-item-action">Attendance</a>
-            <a href="membership.php" class="list-group-item list-group-item-action">Membership</a>
-            <a href="announcements.php" class="list-group-item list-group-item-action">Announcements</a>
-        </div>
-    </div>
-</div>
+
+<!-- SIDEBAR -->
+<section id="sidebar">
+    <a href="#" class="brand">
+        <img src="img/logo.jpg" alt="AdminHub" class="adminhub-image" style="width: 200px; height: auto;">
+    </a>
+
+    <ul class="side-menu top">
+        <li class="active">
+            <a href="#">
+                <i class='bx bxs-dashboard'></i>
+                <span class="text">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-shopping-bag-alt'></i>
+                <span class="text">My Store</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-doughnut-chart'></i>
+                <span class="text">Analytics</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-message-dots'></i>
+                <span class="text">Message</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bxs-group'></i>
+                <span class="text">Team</span>
+            </a>
+        </li>
+    </ul>
+    <ul class="side-menu">
+        <li>
+            <a href="#">
+                <i class='bx bxs-cog'></i>
+                <span class="text">Settings</span>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="logout">
+                <i class='bx bxs-log-out-circle'></i>
+                <span class="text">Logout</span>
+            </a>
+        </li>
+    </ul>
+</section>
+<!-- SIDEBAR -->
