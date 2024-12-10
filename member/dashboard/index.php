220a1d61a5a8a11118f8ced567993f4896ee69b5
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-10 13:09:49 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -37,7 +37,11 @@ if ($endDate) {
 
 	<!-- Boxicons -->
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<!-- My CSS -->
 	
 	<link rel="stylesheet" href="includes/styles.css">
