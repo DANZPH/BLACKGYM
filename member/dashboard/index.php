34a15ff7c5414f6260d07fb3f1d903742e718284
Kent Dancel <128765701+DANZPH@users.noreply.github.com>
2024-12-10 12:35:31 +0800

    update member/dashboard/index.php

--- member/dashboard/index.php
+++ member/dashboard/index.php
@@ -48,10 +48,26 @@ include 'includes/sidebar.php';
 ?>
 	<!-- CONTENT -->
 	<section id="content">
-		<!-- NAVBAR -->
-		<?php
-include 'includes/headbar.php';
-?>
+	<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu' ></i>
+			<a href="#" class="nav-link">Categories</a>
+			<form action="#">
+				<div class="form-input">
+					<input type="search" placeholder="Search...">
+					<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+				</div>
+			</form>
+			<input type="checkbox" id="switch-mode" hidden>
+			<label for="switch-mode" class="switch-mode"></label>
+			<a href="#" class="notification">
+				<i class='bx bxs-bell' ></i>
+				<span class="num">8</span>
+			</a>
+			<a href="#" class="profile">
+				<img src="img/people.png">
+			</a>
+		</nav>
 		<!-- NAVBAR -->
 
 		<!-- MAIN -->
